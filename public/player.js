ccb54a85da90c4bef1cb03426ffb5d53ae26284e
Quinn1876 <qhodges0442@gmail.com>
2021-08-30 22:53:44 -0600

    added tests to player

--- public/player.js
+++ public/player.js
@@ -17,22 +17,28 @@ class Player {
    *
    * @param {number} oldIndex the index of the card you want to move
    * @param {number} newIndex the index of the slot you want to move the card to
+   * The new index is the index of the card to the right of the position where the card
+   * is being inserted
    */
   changeCardPosition(oldIndex, newIndex) {
-    if (typeof oldIndex !== 'number' || typeof newIndex !== number) {
+    if (typeof oldIndex !== 'number' || typeof newIndex !== 'number') {
       console.error('A non number argument was passed in');
     }
 
-    if (oldIndex < 0 || oldIndex >= this.hand.length || newIndex > this.hand.length || newIndex < 0) {
+    if (oldIndex < 0 || oldIndex >= this.hand.length || newIndex >= this.hand.length || newIndex < -1) {
       console.error('Invalid Indices for old and newIndex');
       return;
     }
     if (oldIndex === newIndex) return;
+
     const card = this.hand[oldIndex];
-    if (newIndex === 0) {
+
+    if (newIndex === -1) {
       this.hand = [this.hand[oldIndex], ...this.hand.filter((_, index) => oldIndex !== index)];
-    } else if (newIndex === this.hand.length) {
+
+    } else if (newIndex === this.hand.length - 1) {
       this.hand = [...this.hand.filter((_, index) => oldIndex !== index), this.hand[oldIndex]];
+
     } else if (oldIndex > newIndex) {
       this.hand = this.hand.filter((_, index) => index !== oldIndex);
       this.hand = [
@@ -40,6 +46,7 @@ class Player {
         card,
         ...this.hand.slice(newIndex, this.hand.length),
       ];
+
     } else {
       this.hand = this.hand.filter((_, index) => index !== oldIndex);
       this.hand = [
--- tests/public/player.test.js
+++ tests/public/player.test.js
@@ -6,4 +6,25 @@ test("Adding Cards", () => {
   player.addCardToHand(1);
   expect(player.hand.length).toBe(1);
   expect(player.hand[0]).toBe(1);
+
+  player.addCardToHand(7);
+  expect(player.hand.length).toBe(2);
+  expect(player.hand[1]).toBe(7);
+});
+
+test('Sorting Cards in hand', () => {
+  const player = new Player();
+  player.addCardToHand(0);
+  player.addCardToHand(1);
+  player.addCardToHand(2);
+  player.addCardToHand(3);
+  player.addCardToHand(4);
+  player.addCardToHand(5);
+  player.addCardToHand(6);
+
+  player.changeCardPosition(0, 6);
+  expect(player.hand).toMatchObject([1,2,3,4,5,6,0]);
+
+  player.changeCardPosition(4, -1);
+  expect(player.hand).toMatchObject([5,1,2,3,4,6,0]);
 })
